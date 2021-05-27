e1b648ab44ff63f19b9b882c762fab6c328f0f33
Myskill.PC <orinbay@kolesa.kz>
2021-05-27 17:35:07 +0600

    добавил селекторы

--- lesson4/tests/acceptance/CheckTextCest.php
+++ lesson4/tests/acceptance/CheckTextCest.php
@@ -2,12 +2,11 @@
 use \Codeception\Util\Locator;
 class CheckTextCest
 {
-public function _before(AcceptanceTester $I)
-{
-}
-//tests
 public function checkTextThroughtModalWindow(AcceptanceTester $I)
 {
+    $homeFeaturedXpath=('//ul[2]//li[2]');
+    $indexXpath=('//div[@class="fancybox-wrap fancybox-desktop fancybox-type-iframe fancybox-opened"]');
+    $quickViewXpath=('//*[@id="homefeatured"]/li[2]/div/div[1]/div/a[2]/span');
     $I->amOnPage('');
     $I->scrollTo('#homefeatured > li:nth-child(2)', -300, 0);
     $I->moveMouseOver('#homefeatured > li:nth-child(2)');
@@ -16,9 +15,8 @@ public function checkTextThroughtModalWindow(AcceptanceTester $I)
     $I->click('#homefeatured > li.ajax_block_product.col-xs-12.col-sm-4.col-md-3.last-item-of-mobile-line.hovered > div > div.left-block > div > a.quick-view');
     $I->waitForElementVisible('#index > div.fancybox-overlay.fancybox-overlay-fixed > div > div');
     codecept_debug($I->grabTextFrom('#index > div.fancybox-overlay.fancybox-overlay-fixed > div > div'));
-    #$text = $I->grabTextFrom('//*[@id="product"]/div/div/div[2]/h1');
-    #$I->assertEquals($text, 'Blouse');
     $I->see('Blouse'); 
     
 }
 }
+
--- lesson4/tests/functional/CheckSearchCest.php
+++ lesson4/tests/functional/CheckSearchCest.php
@@ -1,20 +1,18 @@
 <?php 
 class CheckSearchCest
 {
-public function _before(FunctionalTester $I)
-{
-}
-//tests
+
 public function checkTextThroughtModalWindoww(FunctionalTester $I)
 {
+    $searchQueryXpath=('//input[@id="search_query_top"]');
+    $searchBoxXpath= ('//button[@name="submit_search"]');
+    $productContainerXpath=('//div[@class="product-container"]');
     $I->amOnPage('');
     $I->seeElement('#search_query_top');
     $I->click('#search_query_top');
     $I->fillfield('#search_query_top', 'Printed dress');
     $I->seeElement('#searchbox > button');
     $I->click('#searchbox > button');
-    $I->seeNumberOfElements('.product-container', 5);
-  
-    #codecept_debug($I->grabTextFrom('#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1'));
-}
+    $I->seeNumberOfElements('.product-container', 5); 
+  }
 }
